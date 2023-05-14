7575634809fb03a0b4945effe08dde46240c951f
SiposPtr <sipos.peter@sysconet.hu>
2023-05-14 20:57:32 +0200

    erfg

--- Sources/CVEchecker/CVEchecker.swift
+++ Sources/CVEchecker/CVEchecker.swift
@@ -1,56 +1,34 @@
 import Alamofire
 import Foundation
-public class CVE: Codable {
-    public let cve: String
-    public let severity: String
-    public let publicDate: String
-    public let advisories: [String]
-    public let bugzilla: String
-    public let bugzillaDescription: String
-    public let cvssScore: Double?
-    public let cvssScoringVector: String?
-    public let cwe: String
-    public let affectedPackages: [String]
-    public let resourceUrl: String
-    public let cvss3ScoringVector: String
-    public let cvss3Score: Double
-    
-    enum CodingKeys: String, CodingKey {
-        case cve = "CVE"
-        case severity
-        case publicDate = "public_date"
-        case advisories
-        case bugzilla
-        case bugzillaDescription = "bugzilla_description"
-        case cvssScore = "cvss_score"
-        case cvssScoringVector = "cvss_scoring_vector"
-        case cwe = "CWE"
-        case affectedPackages = "affected_packages"
-        case resourceUrl = "resource_url"
-        case cvss3ScoringVector = "cvss3_scoring_vector"
-        case cvss3Score = "cvss3_score"
-    }
-}
+
 public struct CVEchecker {
     public init() {}
     
-    public func getCVEs(completion: @escaping ([CVE]?, Error?) -> Void) {
-        let url = URL(string: "https://example.com/cve_data.json")! // Replace with your URL
-        
-        URLSession.shared.dataTask(with: url) { data, _, error in
-            guard let data = data else {
+    public func getCVEs(package: String, after: String, completion: @escaping ([[String: Any]]?, Error?) -> Void) {
+        let baseURL = "https://access.redhat.com/labs/securitydataapi/cve.json"
+        let parameters: Parameters = ["package": package, "after": after]
+        AF.request(baseURL, parameters: parameters).responseJSON { response in
+            switch response.result {
+            case .success(let value):
+                guard let jsonData = try? JSONSerialization.data(withJSONObject: value, options: []) else {
+                    completion(nil, NSError(domain: "ParsingError", code: 0, userInfo: [NSLocalizedDescriptionKey: "Failed to convert JSON object to data"]))
+                    return
+                }
+
+                do {
+                    if let cves = try JSONSerialization.jsonObject(with: jsonData, options: []) as? [[String: Any]] {
+                        completion(cves, nil)
+                    } else {
+                        completion(nil, NSError(domain: "ParsingError", code: 0, userInfo: [NSLocalizedDescriptionKey: "Failed to parse JSON object"]))
+                    }
+                } catch {
+                    completion(nil, error)
+                }
+
+            case .failure(let error):
                 completion(nil, error)
-                return
             }
-            
-            do {
-                let decoder = JSONDecoder()
-                decoder.keyDecodingStrategy = .convertFromSnakeCase
-                let cves = try decoder.decode([CVE].self, from: data)
-                completion(cves, nil)
-            } catch {
-                completion(nil, error)
-            }
-        }.resume()
+        }
     }
+
 }
--- Tests/CVEcheckerTests/CVEcheckerTests.swift
+++ Tests/CVEcheckerTests/CVEcheckerTests.swift
@@ -4,10 +4,14 @@ class CVEcheckerTests: XCTestCase {
 
     func testGetCVEs() {
         let expectation = self.expectation(description: "GET request should return CVE data")
-        CVEchecker().getCVEs(package: "ios", after: "2000-01-01") { cves, error in
+        CVEchecker().getCVEs(package: "ios", after: "2019-01-01") { cves, error in
+            XCTAssertNotNil(cves, "Expected to receive CVE data")
+            XCTAssertNil(error, "Expected no error to occur")
+            XCTAssertEqual(cves!.count, 11, "Expected to receive 6 CVEs")
             expectation.fulfill()
         }
         
         waitForExpectations(timeout: 5, handler: nil)
     }
+
 }
