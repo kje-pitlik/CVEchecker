e5dbc0334991f0ac056ce3431976d120395ae8e8
SiposPtr <sipos.peter@sysconet.hu>
2023-05-14 21:39:26 +0200

    Update CVEchecker.swift

--- Sources/CVEchecker/CVEchecker.swift
+++ Sources/CVEchecker/CVEchecker.swift
@@ -1,19 +1,29 @@
 import Alamofire
 import Foundation
 
+public struct CVE: Codable {
+    let name: String
+    let cvss3Score: Double
+    let severity: String
+    let description: String
+    let publishedDate: String
+    let modifiedDate: String
+}
+
 public struct CVEchecker {
     public init() {}
     
-    public func getCVEs(package: String, after: String, completion: @escaping ([[String: Any]]?, Error?) -> Void) {
+    public func getCVEs(package: String, after: String, completion: @escaping ([CVE]?, Error?) -> Void) {
         let baseURL = "https://access.redhat.com/labs/securitydataapi/cve.json"
         let parameters: Parameters = ["package": package, "after": after]
         AF.request(baseURL, parameters: parameters).responseJSON { response in
             switch response.result {
             case .success(let value):
-                if let cves = value as? [[String: Any]] {
+                do {
+                    let decoder = JSONDecoder()
+                    let cves = try decoder.decode([CVE].self, from: response.data!)
                     completion(cves, nil)
-                } else {
-                    let error = NSError(domain: "ParsingError", code: 0, userInfo: [NSLocalizedDescriptionKey: "Failed to parse JSON object: \(value)"])
+                } catch let error {
                     completion(nil, error)
                 }
             case .failure(let error):
@@ -21,6 +31,4 @@ public struct CVEchecker {
             }
         }
     }
-
-
 }
