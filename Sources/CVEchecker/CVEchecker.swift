6ccc2b780a67517394647e07876a29a81b6f3794
SiposPtr <sipos.peter@sysconet.hu>
2023-05-14 20:26:16 +0200

    Pushing updates since i have no idea what i am doing

--- Sources/CVEchecker/CVEchecker.swift
+++ Sources/CVEchecker/CVEchecker.swift
@@ -4,13 +4,13 @@ import Foundation
 public struct CVEchecker {
     public init() {}
     
-    public func getCVEs(package: String, after: String, completion: @escaping ([String: Any]?, Error?) -> Void) {
+    public func getCVEs(package: String, after: String, completion: @escaping ([[String: Any]]?, Error?) -> Void) {
         let baseURL = "https://access.redhat.com/labs/securitydataapi/cve.json"
         let parameters: Parameters = ["package": package, "after": after]
         AF.request(baseURL, parameters: parameters).responseJSON { response in
             switch response.result {
             case .success(let value):
-                if let cves = value as? [String: Any] {
+                if let cves = value as? [[String: Any]] {
                     completion(cves, nil)
                 } else {
                     let error = NSError(domain: "ParsingError", code: 0, userInfo: [NSLocalizedDescriptionKey: "Failed to parse JSON object: \(value)"])
@@ -21,4 +21,5 @@ public struct CVEchecker {
             }
         }
     }
+
 }
