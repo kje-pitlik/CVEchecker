b4b15d4e398de0cfb7e9d77cc2d513b273776fb4
SiposPtr <sipos.peter@sysconet.hu>
2023-05-16 13:03:09 +0200

    bdfg

--- Sources/CVEchecker/CVEchecker.swift
+++ Sources/CVEchecker/CVEchecker.swift
@@ -2,30 +2,26 @@ import Alamofire
 import Foundation
 
 public struct CVE: Codable {
-    let name: String
-    let cvss3Score: Double
-    let severity: String
-    let description: String
-    let publishedDate: String
-    let modifiedDate: String
+    public let id: String
+    public let cvssScore: Double
+    public let publishedDate: String
+    public let cwe: String
+    public let summary: String
 }
 
 public struct CVEchecker {
     public init() {}
-
+    
     public func getCVEs(package: String, after: String, completion: @escaping ([CVE]?, Error?) -> Void) {
         let baseURL = "https://access.redhat.com/labs/securitydataapi/cve.json"
         let parameters: Parameters = ["package": package, "after": after]
         AF.request(baseURL, parameters: parameters).responseJSON { response in
             switch response.result {
             case .success(let value):
-                if let cves = value as? [[String: Any]] {
                 do {
                     let decoder = JSONDecoder()
                     let cves = try decoder.decode([CVE].self, from: response.data!)
                     completion(cves, nil)
-                } else {
-                    let error = NSError(domain: "ParsingError", code: 0, userInfo: [NSLocalizedDescriptionKey: "Failed to parse JSON object: \(value)"])
                 } catch let error {
                     completion(nil, error)
                 }
@@ -34,6 +30,4 @@ public struct CVEchecker {
             }
         }
     }
-
-
 }
--- Tests/CVEcheckerTests/CVEcheckerTests.swift
+++ Tests/CVEcheckerTests/CVEcheckerTests.swift
@@ -5,16 +5,4 @@ import Alamofire
 
 class CVEcheckerTests: XCTestCase {
     
-    func testGetCVEs() {
-        let expectation = self.expectation(description: "API call successful")
-        let package = "openssl"
-        let after = "2021-01-01T00:00:00Z"
-        CVEchecker().getCVEs(package: package, after: after) { cveArray in
-            // Assert that the returned array of CVEs is not empty
-            XCTAssertFalse(cveArray.isEmpty)
-            expectation.fulfill()
-        }
-        
-        waitForExpectations(timeout: 5, handler: nil)
-    }
 }
